7defefd685024203192421706398db13683dded7
DanielPetrov802 <145642298+DanielPetrov802@users.noreply.github.com>
2024-11-17 11:17:55 +0200

    added subject menu

--- main_menu.gd
+++ main_menu.gd
@@ -8,3 +8,12 @@ func _on_quit_pressed() -> void:
 
 func _on_options_pressed() -> void:
 	get_tree().change_scene_to_file("res://options_menu.tscn")
+
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://subject_menu.tscn")
+
+
+func _on_back_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://menu.tscn")
